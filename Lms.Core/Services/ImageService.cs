24f55c9e574089fb39885093205a2fa81403f3bc
m.roueintan518@gmail.com <m.roueintan518@gmail.com>
2022-04-09 12:21:00 +0430

    iimageservice & imageservice is added

---
+++ Lms.Core/Services/ImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lms.Core.Services.Interfaces;
+
+namespace Lms.Core.Services
+{
+    public class ImageService:IImageService
+    {
+    }
+}
